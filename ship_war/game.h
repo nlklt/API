b49103e5429fa254a3b1e318938740216c4e4f0f
nlklt <skdin@gmail.com>
2025-10-29 00:24:47 +0300

    0

--- ship_war/game.h
+++ ship_war/game.h
@@ -1,3 +1,8 @@
 #pragma once
 
-void drawBoards();
+const int WIDTH = 11;
+const int HEIGHT = 11;
+
+enum { A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9 };
+
+void drawBoards(const int& ship_boards, const int& shots_board);
--- ship_war/main.cpp
+++ ship_war/main.cpp
@@ -1,8 +1,10 @@
+#include "game.h"
+
 #include <iostream>
 #include <corecrt_io.h>
 #include <fcntl.h>
 
-int ship_board[11][11], shots_board[11][11];
+int ship_board[HEIGHT][WIDTH], shots_board[11][11];
 
 int main()
 {
